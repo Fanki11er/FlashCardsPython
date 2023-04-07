f15d141fc30d0ce440e7a5a31af66042407a23d3
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2023-04-07 21:13:16 +0200

    Fixed models

--- pythonProject/FlashCards/models.py
+++ pythonProject/FlashCards/models.py
@@ -1,19 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
+STATUSES = (
+    ("NEW", "Card not learned"),
+    ("LEARNED", "Card learned")
+)
 
 class UserSettings(models.Model):
-    dailyFlashcards = models.PositiveBigIntegerField()
-    maximumBreak = models.PositiveBigIntegerField()
-    percentNew = models.PositiveIntegerField()
+    daily_flashcards = models.PositiveIntegerField(default=25)
+    maximum_break = models.PositiveIntegerField(default=30)
+    percent_new = models.PositiveIntegerField(default=30)
     user = models.ForeignKey(User, on_delete= models.CASCADE)
 
 
 class FlashCard(models.Model):
-    frontText = models.CharField(max_length= 255)
-    backText = models.CharField(max_length= 255)
-    status = models.BooleanField(default= 'false')
-    correctAtRow = models.PositiveIntegerField()
-    nextSession = models.DateTimeField()
+    front_text = models.CharField(max_length= 255)
+    back_text = models.CharField(max_length= 255)
+    status = models.CharField(choices=(STATUSES), default='NEW', max_length=15)
+    correct_at_row = models.PositiveIntegerField()
+    next_session = models.DateField(default=timezone.now)
     user = models.ForeignKey(User, on_delete= models.CASCADE)
+
+# Flashcard(id: INTEGER, front_text: VARCHAR, back_text: VARCHAR, correct_at_row: INTEGER UNSIGNED, next_session: DATE, status: VARCHAR, user_id: INTEGER)
+# UserSettings (id: INTEGER, maximum_break: INTEGER UNSIGNED, percent_new: INTEGER UNSIGNED, daily_flashcards: INTEGER UNSIGNED, user_id: INTEGER)
