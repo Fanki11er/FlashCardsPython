8367f1ba4a798cddc4b4adc0c548dca1abac4ab4
ewela <ewela28@poczta.onet.pl>
2023-04-07 18:51:59 +0200

    Added more options to admin panel

--- pythonProject/FlashCards/admin.py
+++ pythonProject/FlashCards/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import FlashCard, UserSettings
 # Register your models here.
 
-admin.site.register(FlashCard)
-admin.site.register(UserSettings)
 
+@admin.register(FlashCard)
+class FlashCardAdmin(admin.ModelAdmin):
+    list_display = ['id', 'frontText', 'backText', 'status', 'correctAtRow', 'nextSession', 'user']
+
+    
+
+
+@admin.register(UserSettings)
+class UserSettingsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'dailyFlashcards', 'maximumBreak', 'percentNew', 'user']
+    
